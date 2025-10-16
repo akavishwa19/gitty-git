06532af3b2433c310c1fc34e1f8691359ae72992
Akash <akash19102001@gmail.com>
2025-10-16 11:06:03 +0530

    chore: downgrade matching dependencies
    $fix: correct env conversion from string to number

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors=require("cors");
+const chalk = require("chalk");
+const axios = require("axios");
 require("dotenv").config({quiet:true});
 
 const app = express();
@@ -18,8 +20,19 @@ app.get("/health", async (req, res) => {
   }
 });
 
-const PORT = parseInt(process.env.PORT) || 3001;
+async function fetchVIaAxios(){
+  try {
+    const data =await axios.get('http://localhost:'+PORT+'/health');
+    console.log(data?.data);
+  } catch (error) {
+    console.log(error);
+    throw new Error(error)
+  }
+}
 
-app.listen(PORT, () => {
-  console.log("server listens on http://localhost:" + PORT);
+const PORT = Number(process.env.PORT) || 3001;
+
+app.listen(PORT, async () => {
+ console.log( "app is running on " + chalk.blue.bold("http://localhost:" + PORT));
+ await fetchVIaAxios()
 });
