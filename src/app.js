7da9384c061ebb9dfcd3dd8a0f8ff51ad2e884d8
akavishwacodes <akavishwacodes@gmail.com>
2025-10-18 08:24:23 +0000

    style: add logs

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
+//import packages
 import express from "express";
 import cors from "cors"
 import chalk from "chalk";
 import axios from "axios";
 import 'dotenv/config'
 
+//init app
 const app = express();
 
+//attach middlewares
 app.use(express.json());
 app.use(cors());
 
+//health check api
 app.get("/health", async (req, res) => {
   try {
     const messsage = "server running fine";
@@ -20,6 +24,7 @@ app.get("/health", async (req, res) => {
   }
 });
 
+//dummy function to test health api via axios
 async function fetchVIaAxios(){
   try {
     const data =await axios.get('http://localhost:'+PORT+'/health');
@@ -30,8 +35,10 @@ async function fetchVIaAxios(){
   }
 }
 
+//inject port from env
 const PORT = Number(process.env.PORT) || 3001;
 
+//listen on the port
 app.listen(PORT, async () => {
  console.log( "app is running on " + chalk.blue.underline.bold("http://localhost:" + PORT));
  await fetchVIaAxios()
